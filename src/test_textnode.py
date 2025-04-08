e2ed5e4bc0a4773d860a5973da9ddb017d661c7d
j-abela <jacobabela@gmail.com>
2025-04-09 01:50:34 +0800

    Refactored enum in texttype, wrote more unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -3,28 +3,28 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq_text(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT)
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
     def test_not_eq_text(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT)
-        node2 = TextNode("This is a different text node", TextType.BOLD_TEXT)
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
     def test_not_eq_texttype(self):
-        node = TextNode("This is a text node", TextType.ITALIC_TEXT)
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)    
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.jacob.com.au")         
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.jacob.com.au")
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.jacob.com.au")         
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://www.jacob.com.au")
         self.assertEqual(node, node2)
     
     def test_not_eq_url(self):
-        node = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.jacob.com.au")         
-        node2 = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.jacob.com")
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.jacob.com.au")         
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://www.jacob.com")
         self.assertNotEqual(node, node2)
 
 if __name__ == "__main__":
