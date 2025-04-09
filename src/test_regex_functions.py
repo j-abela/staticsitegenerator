cf4a506645c741548435f22c7e4e9553018cbc2e
j-abela <jacobabela@gmail.com>
2025-04-09 17:02:57 +0800

    implemented regex extractor functions and associated unit tests

---
+++ src/test_regex_functions.py
@@ -0,0 +1,19 @@
+import unittest
+from utilities import *
+
+class TestExtractImagesAndLinks(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        correct_matches = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertListEqual(matches, correct_matches)
+        
+if __name__ == "__main__":
+    unittest.main()    
--- src/utilities.py
+++ src/utilities.py
@@ -1,3 +1,4 @@
+import re
 from htmlnode import LeafNode
 from textnode import TextNode, TextType
 
@@ -44,3 +45,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(TextNode(segments[i], current_text_type))
             
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches 
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches 
