f22703e0927bc1ed53ae15a5cd3e702e12a14ff0
j-abela <jacobabela@gmail.com>
2025-04-09 17:21:35 +0800

    implemented test_to_textnodes function, wrote unit test

---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,23 @@
+import unittest
+from utilities import *
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_all(self):
+        str = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        correct_nodes = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        test_nodes = text_to_textnodes(str)
+        self.assertListEqual(correct_nodes, test_nodes)
+
+if __name__ == "__main__":
+    unittest.main()
